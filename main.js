8e3f41677eb76b5d9b8ec502e07d7c4cd6e7baaa
doyoung1002 <leedoyoung.oc@gmail.com>
2023-04-26 19:29:24 +0900

    JS: Project filtering

--- main.js
+++ main.js
@@ -5,8 +5,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
@@ -53,6 +51,61 @@ document.addEventListener('scroll', () => {
 arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 })
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const ProjectContainer = document.querySelector('.work__projects')
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  projectsContainer.classList.add("anim-out");
+
+
+
+  projects.forEach((project) => {
+
+    const type = project.dataset.type;
+
+
+
+    if (filter === "*" || filter === type) {
+
+      project.classList.remove("invisible");
+
+    }
+
+    else {
+
+      project.classList.add("invisible");
+
+    }
+
+  });
+
+  setTimeout(() => {
+
+    projectsContainer.classList.remove("anim-out");
+
+  }, 300);
+
+
+});
+
+// console.log('-----------------')
+// for (let project of projects) {
+//   console.log(project);
+// }
+
+// console.log('-----------------')
+// let project;
+// for (let i = 0; i < projects.length; i++) {
+//   project = projects[i];
+//   console.log(project);
+// }
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
