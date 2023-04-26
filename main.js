0e4f2fc561e9782eb70f25530426e50fdbc5a6bd
doyoung1002 <leedoyoung.oc@gmail.com>
2023-04-26 19:53:40 +0900

    JS: Navbar menu toggle + project UI tweek

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // Make navbar transparent when it is on the top
-
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -12,6 +11,8 @@ document.addEventListener('scroll', () => {
   }
 });
 
+
+
 // Handle scrolling when tapping on te navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -20,9 +21,15 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+})
 
 // Handle click on 'contact me' button on home
 const homeContactBtn = document.querySelector('.home__contact');
